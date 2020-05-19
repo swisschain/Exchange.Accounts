001df00b7f922292d73452f9561a5fe21892bdc1
Alexey Zelenin <alexey@zelenin.org>
2020-05-19 21:22:39 +0300

    Add transaction to account creation, convert to using statements

--- src/Accounts.Domain.Repositories/Repositories/AccountRepository.cs
+++ src/Accounts.Domain.Repositories/Repositories/AccountRepository.cs
@@ -30,133 +30,131 @@ namespace Accounts.Domain.Persistence.Repositories
 
         public async Task<IReadOnlyList<Account>> GetAllAsync(string brokerId)
         {
-            using (var context = _connectionFactory.CreateDataContext())
-            {
-                var entities = await context.Accounts
-                    .Where(x => x.BrokerId.ToUpper() == brokerId.ToUpper())
-                    .ToListAsync();
+            await using var context = _connectionFactory.CreateDataContext();
 
-                return _mapper.Map<List<Account>>(entities);
-            }
+            var entities = await context.Accounts
+                .Where(x => x.BrokerId == brokerId)
+                .ToListAsync();
+
+            return _mapper.Map<Account[]>(entities);
         }
         
         public async Task<IReadOnlyList<Account>> GetAllAsync(string brokerId, string name, bool? isEnabled,
             ListSortDirection sortOrder = ListSortDirection.Ascending, long cursor = 0, int limit = 50)
         {
-            using (var context = _connectionFactory.CreateDataContext())
+            await using var context = _connectionFactory.CreateDataContext();
+
+            IQueryable<AccountEntity> query = context.Accounts;
+
+            query = query.Where(x => x.BrokerId.ToUpper() == brokerId.ToUpper());
+
+            if (!string.IsNullOrEmpty(name))
+                query = query.Where(x => EF.Functions.ILike(x.Name, $"%{name}%"));
+
+            if (isEnabled.HasValue)
+                query = query.Where(x => x.IsEnabled == isEnabled.Value);
+
+            if (sortOrder == ListSortDirection.Ascending)
             {
-                IQueryable<AccountEntity> query = context.Accounts;
+                if (cursor > 0)
+                    query = query.Where(x => x.Id >= cursor);
 
-                query = query.Where(x => x.BrokerId.ToUpper() == brokerId.ToUpper());
-
-                if (!string.IsNullOrEmpty(name))
-                    query = query.Where(x => EF.Functions.ILike(x.Name, $"%{name}%"));
-
-                if (isEnabled.HasValue)
-                    query = query.Where(x => x.IsEnabled == isEnabled.Value);
-
-                if (sortOrder == ListSortDirection.Ascending)
-                {
-                    if (cursor > 0)
-                        query = query.Where(x => x.Id >= cursor);
-
-                    query = query.OrderBy(x => x.Id);
-                }
-                else
-                {
-                    if (cursor > 0)
-                        query = query.Where(x => x.Id < cursor);
-
-                    query = query.OrderByDescending(x => x.Id);
-                }
-
-                query = query.Take(limit);
-
-                var entities = await query.ToListAsync();
-
-                return _mapper.Map<Account[]>(entities);
+                query = query.OrderBy(x => x.Id);
             }
+            else
+            {
+                if (cursor > 0)
+                    query = query.Where(x => x.Id < cursor);
+
+                query = query.OrderByDescending(x => x.Id);
+            }
+
+            query = query.Take(limit);
+
+            var entities = await query.ToListAsync();
+
+            return _mapper.Map<Account[]>(entities);
         }
 
         public async Task<Account> GetByIdAsync(long id, string brokerId)
         {
-            using (var context = _connectionFactory.CreateDataContext())
-            {
-                IQueryable<AccountEntity> query = context.Accounts;
+            await using var context = _connectionFactory.CreateDataContext();
+            
+            IQueryable<AccountEntity> query = context.Accounts;
 
-                query = query.Where(x => x.BrokerId.ToUpper() == brokerId.ToUpper());
+            query = query.Where(x => x.BrokerId.ToUpper() == brokerId.ToUpper());
 
-                query = query.Where(x => x.Id == id);
+            query = query.Where(x => x.Id == id);
 
-                var entity = await query.SingleOrDefaultAsync();
+            var entity = await query.SingleOrDefaultAsync();
 
-                return _mapper.Map<Account>(entity);
-            }
+            return _mapper.Map<Account>(entity);
         }
 
         public async Task<Account> InsertAsync(Account account)
         {
-            using (var context = _connectionFactory.CreateDataContext())
-            {
-                var entity = _mapper.Map<AccountEntity>(account);
+            await using var context = _connectionFactory.CreateDataContext();
 
-                entity.Created = DateTime.UtcNow;
-                entity.Modified = entity.Created;
+            await using var transaction = await context.Database.BeginTransactionAsync();
 
-                await context.Accounts.AddAsync(entity);
+            var entity = _mapper.Map<AccountEntity>(account);
 
-                await context.SaveChangesAsync();
+            entity.Created = DateTime.UtcNow;
+            entity.Modified = entity.Created;
 
-                var fundingWallet = new WalletEntity(entity.Id, "Funding", WalletType.Funding, true);
+            await context.Accounts.AddAsync(entity);
 
-                await context.Wallets.AddAsync(fundingWallet);
+            await context.SaveChangesAsync();
 
-                var tradingWallet = new WalletEntity(entity.Id, "Trading", WalletType.Trading, true);
+            var fundingWallet = new WalletEntity(entity.Id, WalletType.Funding.ToString(), WalletType.Funding, true);
 
-                await context.Wallets.AddAsync(tradingWallet);
+            await context.Wallets.AddAsync(fundingWallet);
 
-                await context.SaveChangesAsync();
+            var tradingWallet = new WalletEntity(entity.Id, WalletType.Trading.ToString(), WalletType.Trading, true);
 
-                _logger.LogInformation("Account has been created {@context}", entity);
+            await context.Wallets.AddAsync(tradingWallet);
 
-                return _mapper.Map<Account>(entity);
-            }
+            await context.SaveChangesAsync();
+
+            await transaction.CommitAsync();
+
+            _logger.LogInformation("Account has been created {@context}", entity);
+
+            return _mapper.Map<Account>(entity);
         }
 
         public async Task<Account> UpdateAsync(Account account)
         {
-            using (var context = _connectionFactory.CreateDataContext())
-            {
-                var entity = await GetAsync(account.Id, account.BrokerId, context);
+            await using var context = _connectionFactory.CreateDataContext();
 
-                // save fields that has not be updated
-                var created = entity.Created;
+            var entity = await GetAsync(account.Id, account.BrokerId, context);
 
-                _mapper.Map(account, entity);
+            // save fields that has not be updated
+            var created = entity.Created;
 
-                // restore fields that has not be updated
-                entity.Created = created;
+            _mapper.Map(account, entity);
 
-                entity.Modified = DateTime.UtcNow;
+            // restore fields that has not be updated
+            entity.Created = created;
 
-                await context.SaveChangesAsync();
+            entity.Modified = DateTime.UtcNow;
 
-                _logger.LogInformation("Account has been updated {@context}", entity);
+            await context.SaveChangesAsync();
 
-                return _mapper.Map<Account>(entity);
-            }
+            _logger.LogInformation("Account has been updated {@context}", entity);
+
+            return _mapper.Map<Account>(entity);
         }
 
         public async Task DeleteAsync(long id, string brokerId)
         {
-            using (var context = _connectionFactory.CreateDataContext())
-            {
-                var existed = await GetAsync(id, brokerId, context);
+            await using var context = _connectionFactory.CreateDataContext();
 
-                context.Remove(existed);
+            var existed = await GetAsync(id, brokerId, context);
 
-                await context.SaveChangesAsync();
-            }
+            context.Remove(existed);
+
+            await context.SaveChangesAsync();
         }
 
         private async Task<AccountEntity> GetAsync(long id, string brokerId, DataContext context)
