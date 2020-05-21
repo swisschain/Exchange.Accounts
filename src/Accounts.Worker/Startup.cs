080a459aeb8cf586dfd7b7b802c0655cd75e48fb
Alexey Zelenin <alexey@zelenin.org>
2020-05-21 16:42:48 +0300

    Return Worker project

---
+++ src/Accounts.Worker/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Accounts.Common.Configuration;
+using Swisschain.Sdk.Server.Common;
+
+namespace Accounts.Worker
+{
+    public sealed class Startup : SwisschainStartup<AppConfig>
+    {
+        public Startup(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override void ConfigureServicesExt(IServiceCollection services)
+        {
+            base.ConfigureServicesExt(services);
+
+            services.AddHttpClient();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    cfg.UseMessageRetry(y =>
+                        y.Exponential(5,
+                            TimeSpan.FromMilliseconds(100),
+                            TimeSpan.FromMilliseconds(10_000),
+                            TimeSpan.FromMilliseconds(100)));
+
+                    cfg.SetLoggerFactory(provider.GetRequiredService<ILoggerFactory>());
+                }));
+            });
+        }
+    }
+}
